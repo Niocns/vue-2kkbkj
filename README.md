1dbf74ec64ec485e251aa9211527a4025b92e8ca
Niocns <qaw900qaw@gmail.com>
2022-08-25 16:12:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-2kkbkj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-2kkbkj)
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+  <center></center><:>
+<noframes></noframes> <acronym title="i
+ "></acronym>
+ <div></div>
+ <plaintext></plaintext>
+ <optgroup></optgroup>
+ <caption></caption>
+ <headers </header>
+ <blockquote></blockquote>
+ <blockquote></blockquote>
+ <blockquote>a
+</blockquote>
+<cite></cite> <button></button> <col>
+<optgroup></optgroup> <acronym title=""></acronym>
+<address>a</address>
+<applet></applet>
